88442f2f31a8da1cdcee265106e90a46633f7adf
Tara Fenton <tarafenton@Taras-MBP.fios-router.home>
2018-02-25 13:08:36 -0500

    logic worked out for winstates, code clean and commented

--- connect4/js/connect4script.js
+++ connect4/js/connect4script.js
@@ -1,41 +1,49 @@
 /////////////////////// CREATE THE BOARD //////////////////////////////
-var rowCount = 5;//6
-var colCount = 5;//7
+var rowCount = 6;//6
+var colCount = 7;//7
 var connect = 4;
 var board = [];
 var player = 1;
 var $containerDiv = $('body').append("<div id='container'></div>");
 var $dropDiv = $('#container').append("<div id='drop'></div>");
+var $boardDiv = $('#container').append("<div id='board'></div>");
 
 function makeBoard(rowCount, colCount) {
   var rows = [];
   for (var i = 0; i < rowCount; i++) {
     for (var j = 0; j < colCount; j++) {
       rows.push(0);
-      $('#container').append("<div class='boxes' id='box" + i+j + "'>" + i+j + "</div>");
+      $('#board').append("<div class='boxes' id='box" + i+j + "'></div>");
     }
     // push the row into the board
     board.push(rows);
     // reset the row
     rows=[];
-    $('#container').append('<br>');
+    $('#board').append('<br>');
   }
 }
+makeBoard(rowCount, colCount);
+
+function makeDropRow() {
+  for (var i = 0; i <= rowCount; i++) {
+    $('#drop').append("<div class='dropRow' id='drop" + i + "'></div>");
+    // adds event listener for clicking on the individual divs
+    $('#drop'+i).on('click', boxClick);
+  }
+}
+makeDropRow();
+
 // select the column to drop the players pick
 function boxClick(evt) {
   //change the player
   if (player === 1) { player = 2; } else { player = 1; }
-  console.log("clicked "+evt.target.id);
   var index = Number(evt.target.id.charAt(4));
   //check the position of the first available from the bottom
   for (var col = rowCount - 1; col >= 0; col--) {
-    console.log(col, board[col][index])
     if (board[col][index] === 0) {
-      console.log('im empty')
       //push the players id in the board array at the open position
       board[col][index] = player;
       // add player red/black style to open div
-      //$('#box'+col+index).text(player);
       $('#box'+col+index).addClass("player"+player);
       break;
     }
@@ -43,42 +51,14 @@ function boxClick(evt) {
   //check for a win
   checkForAWin();
 }
-makeBoard(rowCount, colCount);
-
-function makeDropRow() {
-  for (var i = 0; i < rowCount; i++) {
-    $('#drop').append("<div class='dropRow' id='drop" + i + "'></div>");
-    // adds event listener for clicking on the individual divs
-    $('#drop'+i).on('click', boxClick);
-  }
-}
-makeDropRow();
-// make the board dynamic
-// first check complete
-// var board = [
-//     [0, 0, 0, 0, 0, 0, 0],
-//     [1, 1, 1, 1, 0, 0, 0],
-//     [2, 1, 0, 2, 0, 0, 0],
-//     [2, 0, 1, 0, 2, 0, 0],
-//     [2, 0, 0, 1, 0, 2, 0],
-//     [2, 0, 0, 0, 0, 0, 2]
-// ]
-// edge cases
-// var board = [
-//     [1, 0, 1, 1, 0, 0, 0],
-//     [1, 0, 1, 1, 0, 0, 0],
-//     [1, 0, 0, 2, 0, 0, 1],
-//     [2, 0, 1, 0, 2, 1, 1],
-//     [2, 0, 0, 1, 1, 2, 1],
-//     [2, 0, 0, 1, 2, 2, 2]
-// ]
-
 
 /////////////////////// CHECK FOR WINSTATES ///////////////////////////
 var winner = 0;
-// vertical winStates
+// VERTICAL WIN - dont have to check all the way to the bottom
+// so only check the top rows (rowCount - connect)
+// and check all the columns
 function checkVertical() {
-  // loop through the rows (top - connect)
+  // loop through the rows
     for (var row = 0; row <= rowCount - connect; row++) {
     // loop through the columns
       for (var col = 0; col < colCount; col++) {
@@ -95,8 +75,9 @@ function checkVertical() {
     }
   }
 }
-
-// horizontal winStates
+// HORIZONTAL WIN - dont have to check all the way to the right
+// so check all the rows
+// and only check the left columns (colCount - connect)
 function checkHorizontal() {
   // loop through the rows
   for (var row = 0; row < rowCount; row++) {
@@ -115,11 +96,13 @@ function checkHorizontal() {
     }
   }
 }
-
-// diagonial winStates
+// DIAGONIAL WIN - DOWN
+// dont have to check all the way to the bottom OR right
+// so only check the top rows (rowCount - connect)
+// and only check the left columns (colCount - connect)
 function checkDiagonialDown() {
   // loop through the rows
-  for (var row = 0; row < rowCount; row++) {
+  for (var row = 0; row <= rowCount - connect; row++) {
     // loop through the columns
     for (var col = 0; col <= colCount - connect; col++) {
       // checking for 4 diagonial top down
@@ -135,18 +118,22 @@ function checkDiagonialDown() {
     }
   }
 }
-
-function checkDiagonialUpBackward() {
-  // loop through the rows backwards only until 4 less than top
-  for (var row = rowCount - 1; row > rowCount - connect; row--) {
-    // loop through the columns backwards only until 4 less than left
-    for (var col = rowCount; col > colCount - connect; col--) {
-      // checking for 4 diagonial bottom up
+// DIAGONIAL WIN - UP
+// dont have to check all the way to the top OR right
+// MUST decrement to start at bottom of board on the rows
+// and only check the bottom rows (rowCount - connect)
+// and only check the left columns (colCount - connect)
+function checkDiagonialUp() {
+  // loop through the rows
+  for (var row = rowCount - 1; row >= rowCount - connect; row--) {
+    // loop through the columns
+    for (var col = 0; col <= colCount - connect; col++) {
+      // checking for 4 diagonial up
       //console.log("checking "+board[row][col]);
       if (board[row][col] != 0 &&
-          board[row][col] === board[row-1][col-1] &&
-          board[row][col] === board[row-2][col-2] &&
-          board[row][col] === board[row-3][col-3]) {
+          board[row][col] === board[row-1][col+1] &&
+          board[row][col] === board[row-2][col+2] &&
+          board[row][col] === board[row-3][col+3]) {
         console.log("winner!"+board[row][col])
         winner = board[row][col];
         return board[row][col];
@@ -154,35 +141,14 @@ function checkDiagonialUpBackward() {
     }
   }
 }
-
-function checkDiagonialUpForward() {
-  // loop through the rows backwards only until 4 less than top
-  for (var row = rowCount - 1; row > rowCount - connect; row--) {
-    // loop through the columns backwards only until 4 less than left
-    for (var col = rowCount; col > colCount - connect; col--) {
-      // checking for 4 diagonial bottom up
-      //console.log("checking "+board[row][col]);
-      if (board[row][col] != 0 &&
-          board[row][col] === board[row-1][col-1] &&
-          board[row][col] === board[row-2][col-2] &&
-          board[row][col] === board[row-3][col-3]) {
-        console.log("winner!"+board[row][col])
-        winner = board[row][col];
-        return board[row][col];
-      }
-    }
-  }
-}
-
 function checkForAWin() {
   checkVertical();
   checkHorizontal();
-  //checkDiagonialDown();
-  checkDiagonialUpBackward(); //up backwards
-  checkDiagonialUpForward();
+  checkDiagonialDown();
+  checkDiagonialUp();
   if (winner !== 0) {
     alert("winner! "+winner);
   }
 }
 
-
+// https://stackoverflow.com/questions/15457796/four-in-a-row-logic
