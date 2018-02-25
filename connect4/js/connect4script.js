712756c96c63929eeabe0fd3f4bcd76f78f3cb9a
Tara Fenton <tarafenton@Taras-MBP.fios-router.home>
2018-02-24 22:51:58 -0500

    set variables for rowCount and colCount to adjust loops so it can be more dynamic

--- connect4/js/connect4script.js
+++ connect4/js/connect4script.js
@@ -8,13 +8,26 @@
 //     [2, 0, 0, 1, 0, 2, 0],
 //     [2, 0, 0, 0, 0, 0, 2]
 // ]
+// edge cases
+var board = [
+    [1, 0, 1, 1, 0, 0, 0],
+    [1, 0, 1, 1, 0, 0, 0],
+    [1, 0, 0, 2, 0, 0, 1],
+    [2, 0, 1, 0, 2, 1, 1],
+    [2, 0, 0, 1, 1, 2, 1],
+    [2, 0, 0, 1, 2, 2, 2]
+]
 
+var connect = 4;
 // vertical winStates
 function checkVertical() {
-  // loop through the rows 0 - 2
-  for (var row = 0; row < 3; row++) {
+  // loop through the rows (top - connect)
+    for (var row = 0; row <= rowCount - connect; row++) {
+      //for (var row = 0; row <= 3; row++) {
     // loop through the columns
-    for (var col = 0; col < board.length; col++) {
+      for (var col = 0; col < colCount; col++) {
+    //for (var col = 0; col < board.length; col++) {
+      ////
       // checking for 4 down
       //console.log("checking "+board[row][col]);
       if (board[row][col] != 0 &&
@@ -32,9 +45,11 @@ function checkVertical() {
 // horizontal winStates
 function checkHorizontal() {
   // loop through the rows
-  for (var row = 0; row < board.length; row++) {
+  for (var row = 0; row < rowCount; row++) {
+    console.log(row);
     // loop through the columns
-    for (var col = 0; col < 3; col++) {
+    for (var col = 0; col <= colCount - connect; col++) {
+      console.log(row,col);
       // checking for 4 across
       console.log("checking "+board[row][col]);
       if (board[row][col] != 0 &&
@@ -48,14 +63,15 @@ function checkHorizontal() {
   }
 }
 //checkHorizontal();
-
+var rowCount = 6;
+var colCount = 7;
 // diagonial winStates
 function checkDiagonialDown() {
   // loop through the rows
-  for (var row = 0; row < board.length; row++) {
+  for (var row = 0; row < rowCount; row++) {
     // loop through the columns
-    for (var col = 0; col < 4; col++) {
-      // checking for 4 across
+    for (var col = 0; col <= colCount - connect; col++) {
+      // checking for 4 diagonial top down
       console.log("checking "+board[row][col]);
       if (board[row][col] != 0 &&
           board[row][col] === board[row+1][col+1] &&
@@ -68,12 +84,14 @@ function checkDiagonialDown() {
   }
 }
 //checkDiagonialDown();
+var rowCount = 6;
+var colCount = 7;
 function checkDiagonialUp() {
   // loop through the rows backwards only until 4 less than top
-  for (var row = board.length - 1; row > board.length - 4; row--) {
+  for (var row = rowCount - 1; row > rowCount - connect; row--) {
     // loop through the columns backwards only until 4 less than left
-    for (var col = 6; col > 7 - 4; col--) {
-      // checking for 4 across
+    for (var col = rowCount; col > colCount - connect; col--) {
+      // checking for 4 diagonial bottom up
       console.log("checking "+board[row][col]);
       if (board[row][col] != 0 &&
           board[row][col] === board[row-1][col-1] &&
