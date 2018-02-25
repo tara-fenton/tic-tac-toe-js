f635df51f9c27dca3a0825fe3a114da812ddf776
Tara Fenton <tarafenton@Taras-MBP.fios-router.home>
2018-02-24 23:14:40 -0500

    createBoard and checkForAWin functions created

--- connect4/js/connect4script.js
+++ connect4/js/connect4script.js
@@ -1,3 +1,22 @@
+/////////////////////// CREATE THE BOARD //////////////////////////////
+var rowCount = 6;
+var colCount = 7;
+var connect = 4;
+var board = [];
+function makeBoard(rowCount, colCount) {
+  var rows = [];
+  for (var i = 0; i < rowCount; i++) {
+    for (var j = 0; j < colCount; j++) {
+      rows.push(0);
+    }
+    // push the row into the board
+    board.push(rows);
+    // reset the row
+    rows=[];
+  }
+}
+makeBoard(rowCount, colCount);
+console.log(board);
 // make the board dynamic
 // first check complete
 // var board = [
@@ -9,25 +28,24 @@
 //     [2, 0, 0, 0, 0, 0, 2]
 // ]
 // edge cases
-var board = [
-    [1, 0, 1, 1, 0, 0, 0],
-    [1, 0, 1, 1, 0, 0, 0],
-    [1, 0, 0, 2, 0, 0, 1],
-    [2, 0, 1, 0, 2, 1, 1],
-    [2, 0, 0, 1, 1, 2, 1],
-    [2, 0, 0, 1, 2, 2, 2]
-]
+// var board = [
+//     [1, 0, 1, 1, 0, 0, 0],
+//     [1, 0, 1, 1, 0, 0, 0],
+//     [1, 0, 0, 2, 0, 0, 1],
+//     [2, 0, 1, 0, 2, 1, 1],
+//     [2, 0, 0, 1, 1, 2, 1],
+//     [2, 0, 0, 1, 2, 2, 2]
+// ]
 
-var connect = 4;
+
+/////////////////////// CHECK FOR WINSTATES ///////////////////////////
+var winner = 0;
 // vertical winStates
 function checkVertical() {
   // loop through the rows (top - connect)
     for (var row = 0; row <= rowCount - connect; row++) {
-      //for (var row = 0; row <= 3; row++) {
     // loop through the columns
       for (var col = 0; col < colCount; col++) {
-    //for (var col = 0; col < board.length; col++) {
-      ////
       // checking for 4 down
       //console.log("checking "+board[row][col]);
       if (board[row][col] != 0 &&
@@ -35,34 +53,33 @@ function checkVertical() {
           board[row][col] === board[row+2][col] &&
           board[row][col] === board[row+3][col]) {
         console.log("winner!"+board[row][col])
+        winner = board[row][col];
         return board[row][col];
       }
     }
   }
 }
-//checkVertical();
 
 // horizontal winStates
 function checkHorizontal() {
   // loop through the rows
   for (var row = 0; row < rowCount; row++) {
-    console.log(row);
     // loop through the columns
     for (var col = 0; col <= colCount - connect; col++) {
-      console.log(row,col);
       // checking for 4 across
-      console.log("checking "+board[row][col]);
+      //console.log("checking "+board[row][col]);
       if (board[row][col] != 0 &&
           board[row][col] === board[row][col+1] &&
           board[row][col] === board[row][col+2] &&
           board[row][col] === board[row][col+3]) {
         console.log("winner!"+board[row][col])
+        winner = board[row][col];
         return board[row][col];
       }
     }
   }
 }
-//checkHorizontal();
+
 var rowCount = 6;
 var colCount = 7;
 // diagonial winStates
@@ -72,37 +89,49 @@ function checkDiagonialDown() {
     // loop through the columns
     for (var col = 0; col <= colCount - connect; col++) {
       // checking for 4 diagonial top down
-      console.log("checking "+board[row][col]);
+      //console.log("checking "+board[row][col]);
       if (board[row][col] != 0 &&
           board[row][col] === board[row+1][col+1] &&
           board[row][col] === board[row+2][col+2] &&
           board[row][col] === board[row+3][col+3]) {
         console.log("winner!"+board[row][col])
+        winner = board[row][col];
         return board[row][col];
       }
     }
   }
 }
-//checkDiagonialDown();
-var rowCount = 6;
-var colCount = 7;
+
 function checkDiagonialUp() {
   // loop through the rows backwards only until 4 less than top
   for (var row = rowCount - 1; row > rowCount - connect; row--) {
     // loop through the columns backwards only until 4 less than left
     for (var col = rowCount; col > colCount - connect; col--) {
       // checking for 4 diagonial bottom up
-      console.log("checking "+board[row][col]);
+      //console.log("checking "+board[row][col]);
       if (board[row][col] != 0 &&
           board[row][col] === board[row-1][col-1] &&
           board[row][col] === board[row-2][col-2] &&
           board[row][col] === board[row-3][col-3]) {
         console.log("winner!"+board[row][col])
+        winner = board[row][col];
         return board[row][col];
       }
     }
   }
 }
-checkDiagonialUp();
+
+function checkForAWin() {
+  checkVertical();
+  checkHorizontal();
+  checkDiagonialDown();
+  checkDiagonialUp();
+  if (winner === 0) {
+    console.log("no winner :(")
+  } else {
+    console.log("winner! "+winner)
+  }
+}
+checkForAWin();
 // select the column to drop the players pick
 
